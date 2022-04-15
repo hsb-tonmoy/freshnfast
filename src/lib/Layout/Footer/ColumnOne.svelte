8f9ee77e600bc2b480106d0fdd0e066a43ad7e06
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-04-15 08:12:44 -0400

    Added column two.

--- src/lib/Layout/Footer/ColumnOne.svelte
+++ src/lib/Layout/Footer/ColumnOne.svelte
@@ -1,9 +1,11 @@
-<div id="logo" class="">
-	<a href="/">
-		<img
-			class={`transition-all ease-in-out duration-500 w-32 h-32 md:w-40 md:h-40 lg:w-52 lg:h-52`}
-			src="/images/logo.png"
-			alt="FreshNFast"
-		/>
-	</a>
+<div class="flex flex-col">
+	<div id="logo" class="">
+		<a href="/">
+			<img
+				class={`transition-all ease-in-out duration-500 w-32 h-32 md:w-40 md:h-40 lg:w-52 lg:h-52`}
+				src="/images/logo.png"
+				alt="FreshNFast"
+			/>
+		</a>
+	</div>
 </div>
---
+++ src/lib/Layout/Footer/ColumnTwo.svelte
@@ -0,0 +1,4 @@
+<h6 class="font-semibold uppercase text-sm text-black">Menu</h6>
+<ul class="text-sm">
+	<li class="">Home</li>
+</ul>
