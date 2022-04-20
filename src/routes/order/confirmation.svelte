3e1f05514a5b1638f3b8336d90ffe01c292f6b47
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-04-20 11:27:27 -0400

    Changed margins.

--- src/routes/order/confirmation.svelte
+++ src/routes/order/confirmation.svelte
@@ -14,24 +14,24 @@
 </svelte:head>
 
 <main class="flex h-screen">
-	<div class="sidebar flex flex-col w-[20%] px-12">
+	<div class="sidebar flex flex-col w-[20%] px-12 overflow-y-auto py-6">
 		<div id="logo" class="self-center">
 			<a href="/">
 				<img
-					class={`transition-all ease-in-out duration-500 w-16 h-16 md:w-20 md:h-20 lg:w-32 lg:h-32 mt-8`}
+					class={`transition-all ease-in-out duration-500 w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 mt-8`}
 					src="/images/logo.png"
 					alt="FreshNFast"
 				/>
 			</a>
 		</div>
-		<div class="flex flex-col mt-20">
-			<span class="font-semibold text-3xl text-lightGreen">Thank you for your order!</span>
+		<div class="flex flex-col mt-14">
+			<span class="font-semibold text-2xl text-lightGreen">Thank you for your order!</span>
 			<span class="text-base text-neutral-500 mt-2"
 				>Your order is being prepared. It may take anywhere from 5-15 minutes. We will notify you
 				via email once your order is ready for pickup</span
 			>
 		</div>
-		<div class="receipt flex flex-col mt-20">
+		<div class="receipt flex flex-col mt-14">
 			<h3 class="text-darkGreen text-2xl font-medium">Order Receipt</h3>
 			<hr class="h-[5px] w-[18%] bg-lightGreen mt-2 mb-6" />
 			<div class="inline-flex text-sm gap-x-2">
